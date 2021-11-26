99d5f062a3314db6ec010e6bb5267811deb16b8d
Suria_weh <71096420+SplendidTea@users.noreply.github.com>
2021-11-25 22:53:12 -0500

    Added Queries
    Not sure if they'll work but I hope

---
+++ ex5.sql
@@ -0,0 +1,38 @@
+SELECT * FROM WorkingAt
+LEFT JOIN Hospital ON WorkingAt.address = Hospital.address 
+UNION 
+SELECT * FROM WorkingAt
+RIGHT JOIN Hospital ON WorkingAt.address = Hospital.address;
+
+
+
+SELECT outbreakDistrict, numPeople, numCases, numResolved, num Recovered
+FROM Hotspot
+WHERE UNLOCode = (SELECT UNLOCode FROM City WHERE cityName='London');
+
+SELECT * FROM CovidVarient
+WHERE varientName='delta';
+
+
+SELECT address, clinicName, vaccineAdministrators, 
+clinicStaffTotal-(SELECT AVG(clinicStaffTotal) FROM VaccineClinic) AS cStaffDif
+FROM VaccineClinic
+WHERE clinicStaffTotal >(SELECT AVG(clinicStaffTotal) FROM VaccineClinic);
+
+
+SELECT p.healthCardID, p.personName, v.vaccineName, c.clinicName
+FROM Person p, VaccineShot v, VaccineClinic c
+WHERE p.healthCardID = v.healthCardID AND p.healthCardID= c.healthCardID 
+ORDER BY p.healthCardID, p.personName, c.clinicName, v.vaccineName;
+
+SELECT population-(SELECT COUNT(healthCardID)
+FROM Person
+WHERE doseNumber = 0 AND UNLOCode =(SELECT UNLOCode FROM City WHERE cityName='London')) AS healthyPop
+FROM City
+WHERE cityName='London';
+
+SELECT * 
+FROM Person
+WHERE healthCardID IN 
+(SELECT healthCardID FROM Hospitalized WHERE address= (SELECT address FROM Hospital WHERE hospitalName= 'name here'));
+
