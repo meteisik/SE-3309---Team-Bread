deef97e615db2a17b5fb55c2b2f9b4a12fbb5884
nbroeder <nbroeder@uwo.ca>
2021-12-04 20:59:18 -0500

    Get and display clinic, hospital, and the combined queries.

--- Website/src/Medical.js
+++ Website/src/Medical.js
@@ -1,15 +1,47 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import { Typography } from '@mui/material'
 import Table from "./Table"
+
+
+
+
 function Medical() {
  
+    const [apiResponse, setResponse] = useState('');
+
+    useEffect(() => {
+        getDefaultTable();
+    }, []);
+
+
+    
+
+      function getDefaultTable() {
+        fetch("http://localhost:9000/testAPI/getHealthCentres")
+            .then(res => res.text())
+            .then(res => setResponse(res));
+      }
+      function getClinics() {
+        fetch("http://localhost:9000/testAPI/getClinics")
+            .then(res => res.text())
+            .then(res => setResponse(res));
+      }
+      function getHospitals() {
+        fetch("http://localhost:9000/testAPI/getHospitals")
+            .then(res => res.text())
+            .then(res => setResponse(res));
+      }
 
 
     
     return (
        
 <div id ="message container"> 
-HELLO THIS IS MEDICAL
+
+<button onClick={getClinics}>Clinics</button>
+<button onClick={getHospitals}>Hospitals</button>
+<button onClick={getDefaultTable}>All</button>
+<text>{apiResponse}</text>
 </div>
 
 
--- assignment4/api/routes/TestAPI.js
+++ assignment4/api/routes/TestAPI.js
@@ -8,8 +8,6 @@ router.get("/", function(req, res, next) {
 });
 
 router.get("/covidVarients", function(req, res, next) {
-    
-
     let conn=newConnection();
     conn.connect();
         
@@ -31,6 +29,79 @@ router.get("/covidVarients", function(req, res, next) {
     conn.end();
     
 });
+
+///////////////////////get requests for the medical section\\\\\\\\\\\\\\\\\\\\\\\\\\
+router.get("/getHealthCentres", function(req, res, next) {
+    let conn=newConnection();
+    conn.connect();
+        
+    conn.query(`select hcc.address, c.cityName, v.spotName from healthcarecentre as hcc 
+    JOIN city as c ON hcc.UNLOCode=c.UNLOCode Left Join (select clinicName as spotName, address from vaccineclinic  
+    UNION select hospitalName, address from hospital) as v on v.address=hcc.address; `,(err,row,fields)=>{
+    if(err){
+        console.log(err);
+        }
+        else{
+            let allData={};
+            for(r in row){
+                allData[r]=row[r];
+            }
+            res.send(JSON.stringify(row));
+        }
+    });
+    conn.end();
+    
+});
+
+router.get("/getHospitals", function(req, res, next) {
+    let conn=newConnection();
+    conn.connect();
+        
+    conn.query(`SELECT h.address, h.hospitalName, h.numOfCovidCases,h.bedsAvailable,c.cityName  FROM hospital AS h
+    JOIN healthcarecentre AS hcc
+    ON h.address=hcc.address
+    JOIN city AS c
+    ON hcc.UNLOCode=c.UNLOCode;`,(err,row,fields)=>{
+    if(err){ 
+        console.log(err);
+        }
+        else{
+            let allData={};
+            for(r in row){
+                allData[r]=row[r];
+            }
+            res.send(JSON.stringify(row));
+        }
+    });
+    conn.end();
+    
+});
+
+router.get("/getClinics", function(req, res, next) {
+    let conn=newConnection();
+    conn.connect();
+        
+    conn.query(`SELECT v.address, v.clinicName,c.cityName  FROM vaccineclinic AS v
+    JOIN healthcarecentre AS hcc
+    ON v.address=hcc.address
+    JOIN city AS c
+    ON hcc.UNLOCode=c.UNLOCode;`,(err,row,fields)=>{
+    if(err){ 
+        console.log(err);
+        }
+        else{
+            let allData={};
+            for(r in row){
+                allData[r]=row[r];
+            }
+            res.send(JSON.stringify(row));
+        }
+    });
+    conn.end();
+    
+});
+
+
 
 module.exports = router;
 
