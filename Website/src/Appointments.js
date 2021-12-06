dc1816ab53a78c50bf946f2237ff3a5a0853bacd
Robert <49911127+rfreeman203@users.noreply.github.com>
2021-12-06 10:20:02 -0500

    Main code for appointment

--- Website/src/Appointments.js
+++ Website/src/Appointments.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, Typography } from '@mui/material'
+import { Divider, Grid, Typography } from '@mui/material'
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
@@ -14,72 +14,118 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Container from '@mui/material/Container'
-
+import TableBody from "@mui/material/TableBody";
+import TableContainer from "@mui/material/TableContainer";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
 
 
 
 function Appointments() {
 
-    
     //Get cities
-    let [apiCityResponse, setCityResponse] = React.useState('');
-    fetch("http://localhost:9000/testAPI/cities")
-        .then(res => res.text())
-        .then(res => setCityResponse(res));
+    const [apiCityResponse, setCityResponse] = React.useState([]);
     
+    function getCitiesFromDB() {
+        fetch("http://localhost:9000/testAPI/cities")
+            .then(res => res.text())
+            .then(res => setCityResponse(JSON.parse(res)));
+    }
 
     //format citis for autocomplete input
     function formatCities(){
-        const cityList = JSON.parse(apiCityResponse);
-        console.log(apiCityResponse);
         let cityNames = "";
-        for (let c in cityList){
-            cityNames += cityList[c].cityName + "*";
+        for (let c in apiCityResponse){
+            cityNames += apiCityResponse[c].cityName + "*";
         }
         return cityNames;
     }
-    
     //set values to interact with database and retrieve clinic names
     const [cityValue, setCityValue] = React.useState('');
     const [cityInputValue, setCityInputValue] = React.useState('');
 
 
-    // const[apiClinicResponse, setClinicResponse] = React.useState('');
-    // function getVaccineClinicsFromDB() {
-    //     fetch("http://localhost:9000/testAPI/vaccineClinics")
-    //         .then(res => res.text())
-    //         .then(res => setClinicResponse(res));
-    // }
-    // getVaccineClinicsFromDB();
+    //--------------------------------------------------------------------------------------------------------------
 
-    // //format citis for autocomplete input
-    // function formatClinics(){
-    //     let clinicList = JSON.parse(apiClinicResponse);
-    //     let clinicNames = "";
-    //     for (let c in clinicList){
-    //         clinicNames += clinicList[c] + "*";
-    //     }
-    //     console.log(clinicNames);
-        
-    // }
-    // formatClinics();
-    
+    const[apiClinicResponse, setClinicResponse] = React.useState([]);
+
+    function getVaccineClinicsFromDB(cityVal) {
+		//takes the text box value and adds it to a post request
+        const requestOptions = {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({cityVal})
+        };
+
+        fetch("http://localhost:9000/testAPI/vaccineClinics", requestOptions)
+            .then(res => res.text())
+            .then(res => setClinicResponse(JSON.parse(res)));
+    }
+    function formatClinics(){
+        let clinicInfo = "";
+        console.log(apiClinicResponse);
+        for (let c in apiClinicResponse){
+            console.log(apiClinicResponse[c]);
+            clinicInfo += "" + apiClinicResponse[c].clinicName + "*";
+        }
+        console.log(clinicInfo);
+        return clinicInfo;
+    }
+    function getClinicAddress(val){
+        let clinicAddress = "";
+        for (let c in apiClinicResponse){
+            if(apiClinicResponse[c].clinicName == val)
+                clinicAddress += "" + apiClinicResponse[c].address;
+        }
+        console.log(clinicAddress);
+        return clinicAddress;
+    }
     const [clinicValue, setClinicValue] = React.useState('');
     const [clinicInputValue, setClinicInputValue] = React.useState('');
+    const [clinicAddress, setClinicAddress] = React.useState('');
 
+    //-------------------------------------------------------------------------------------------------------
 
-    
     const Item = styled(Paper)(({ theme }) => ({
         ...theme.typography.body2,
         padding: theme.spacing(1),
         textAlign: 'center',
         color: theme.palette.text.secondary,
-      }));
+    }));
+
+    //------------------------------------------------------------------------------------------------------
+
+    const[apiAppointmentResponse, setAppointmentResponse] = React.useState([]);
+
+    function createAppointment(){
+
+        let hcID = document.getElementById('healthcardID').value;
+        let date = document.getElementById('date').value;
+
+        let AppointmentInfo = {
+            healthCardId : hcID,
+            address : clinicAddress,
+            date: date
+        }
+        alert(JSON.stringify(AppointmentInfo));
+
+        const requestOptions = {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({AppointmentInfo})
+        };
+        
+        fetch("http://localhost:9000/testAPI/appointmentTable", requestOptions)
+            .then(res => res.text())
+            .then(res => setAppointmentResponse(JSON.parse(res)));
+
+    }
+
+    //-------------------------------------------------------------------------------------------------------
 
     return (
-    
-    
-
     <React.Fragment>
 
 
@@ -87,20 +133,20 @@ function Appointments() {
       <Grid container spacing={1}>
         <Grid item xs={2}>
         
-        <Box sx={{ bgcolor: '#ffb851', height: '100vh' }} />
+        <Box sx={{ bgcolor: '#ffb851', height: '140vh' }} />
    
         </Grid>
-
         <Grid item xs={8}>
         <Container >
         <List>
-
+        <ListItem>
+        <button onClick={getCitiesFromDB}>load cities</button>
+        </ListItem>
         <ListItem>
             <Typography variant = "h4" gutterBottom>
             Vaccine Appointment Booking
             </Typography>
         </ListItem>
-
         <ListItem>
             <Typography variant = "h7" gutterBottom>
             The Ontario government is continuing with the rollout of its vaccine implementation plan using a phased approach, 
@@ -109,19 +155,13 @@ function Appointments() {
             an healthcard id to receive your booking confirmation. Please enter your booking information below:
             </Typography>
         </ListItem>
-
-        <ListItem>
-            <div>{`city value: ${cityValue !== null ? `'${cityValue}'` : 'null'}`}</div>
-            <div>{`city inputValue: '${cityInputValue}'`}</div>
-        </ListItem>
-
-        
-        <ListItem>
-            Select a city to book your vaccine:
+        <Divider/>
+        <ListItem>Select a city to book your vaccine:
             <Autocomplete
             value={cityValue}
             onChange={(event, newValue) => {
             setCityValue(newValue);
+            getVaccineClinicsFromDB(newValue);
             }}
             inputValue={cityInputValue}
             onInputChange={(event, newInputValue) => {
@@ -130,70 +170,94 @@ function Appointments() {
             disablePortal
             xs = {4}
             id="combo-box-demo"
-            options={apiCityResponse.split(",")}
-            //options={formatCities().split("*")}
+            //options={apiCityResponse.split(",")}
+            options={formatCities().split("*")}
             sx={{ width: 300 }}
             renderInput={(params) => <TextField {...params} label="cities" />}
             />
         </ListItem>
-
-        <ListItem>
-            <div>{`clinic value: ${clinicValue !== null ? `'${clinicValue}'` : 'null'}`}</div>
-            <div>{`clinic inputValue: '${clinicInputValue}'`}</div>
-        </ListItem>
-
-        {/* <ListItem>
-            Select a hospital in your city {cityValue}:
+        <ListItem>Select a vaccine clinic in your city {cityValue}:
             <Autocomplete
             value={clinicValue}
             onChange={(event, newValue) => {
+            let adressVal = getClinicAddress(newValue);
+            setClinicAddress(adressVal);
             setClinicValue(newValue);
             }}
             inputValue={clinicInputValue}
             onInputChange={(event, newInputValue) => {
             setClinicInputValue(newInputValue);
             }}
+            clinicAddress={clinicAddress}
             disablePortal
             xs = {4}
             id="combo-box-demo"
-            options={formatCities().split("*")}
+            options={formatClinics().split("*")}
             sx={{ width: 300 }}
             renderInput={(params) => <TextField {...params} label="clinics" />}
             />
-        </ListItem> */}
-
-        <ListItem>
-            Please enter a valid healthcard id: 
+        </ListItem>
+        <ListItem>Clinic address: {clinicAddress}
+        </ListItem>
+        <ListItem>Please enter a valid healthcard id: 
             <TextField
                 required
-                id="outlined-required"
+                id="healthcardID"
                 label="Required"
             />
         </ListItem>
-
         <ListItem>Please enter a valid vaccine date (form: YYYY-MM-DD):
             <TextField
                 required
-                id="outlined-required"
+                id="date"
                 label="Required"
             />
         </ListItem>
-
         <ListItem>
-            <Button>Submit</Button>
+            <Button onClick={createAppointment}>Submit</Button>
         </ListItem>
-
+        <Divider />
+        <ListItem>
+        <Typography variant = "h6" gutterBottom>
+            Your Vaccine Appointment:
+        </Typography>
+        </ListItem>
+        <ListItem>
+            <TableContainer component={Paper}>
+                <Table sx={{minWidth: 650}} aria-label="customized table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="right">Name</TableCell>
+                            <TableCell align="right">Date</TableCell>
+                            <TableCell align="right">Address</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {apiAppointmentResponse.map((row, i) => (
+                            <TableRow
+                                key={i}
+                                sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                            >
+                                <TableCell align="right">{apiAppointmentResponse[i].personName}</TableCell>
+                                <TableCell align="right">{apiAppointmentResponse[i].injectionDate}</TableCell>
+                                <TableCell align="right">{apiAppointmentResponse[i].address}</TableCell>
+                            </TableRow>
+                        )
+                        )}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </ListItem>
         </List>
       </Container>
         </Grid>
         <Grid item xs ={2}>
-        <Box sx={{ bgcolor: '#ffb851', height: '100vh' }} />
+        <Box sx={{ bgcolor: '#ffb851', height: '140vh' }} />
         </Grid>
       </Grid>
     </Box>
     </React.Fragment>
     )
 }
-/** */
 
 export default Appointments
--- assignment4/api/routes/TestAPI.js
+++ assignment4/api/routes/TestAPI.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
 const newConnection=require('./DBConnection');
+const mysql=require('mysql');
+
 
 router.get("/", function(req, res, next) {
     
@@ -101,13 +103,14 @@ router.get("/getClinics", function(req, res, next) {
     
 });
 
+
+//##################################  FOR APPOINTMENTS PAGE  #######################################
 router.get("/cities", function(req, res, next) {
     let conn=newConnection();
     conn.connect();
         
     conn.query("SELECT cityName FROM city",(err,row,fields)=>{
     if(err){ 
-        console.log(err);
         }
         else{
             let allData={};
@@ -116,21 +119,26 @@ router.get("/cities", function(req, res, next) {
                 allData[r]=row[r];
             }
 
-            res.send(JSON.stringify(allData));
-            console.log(row[0])
+            res.send(JSON.stringify(row));
         }
     });
     conn.end();
 });
 
-router.get("/vaccineClinics", function(req, res, next) {
+router.post("/vaccineClinics", function(req, res, next) {
+
+    let cities = req.body.cityVal;
+    console.log(req.body.cityVal);
+    console.log(cities);
+
     let conn=newConnection();
     conn.connect();
         
-    conn.query( "SELECT ci.cityName, cl.address FROM healthcarecentre h, vaccineClinic cl, city ci" +
-                "WHERE h.address = cl.address AND ci.cityName = \"Olds \" AND h.UNLOCode = (SELECT UNLOCode FROM City" +
-                "WHERE cityName = \"Olds \""
-    ,(err,row,fields)=>{
+    let queryStatement = `SELECT clinicName, c.address FROM healthcarecentre h, vaccineClinic c
+                WHERE h.address = c.address AND h.UNLOCode = (SELECT UNLOCode FROM City
+                WHERE cityName = ` + mysql.escape(cities) + `)`;
+    
+    conn.query(queryStatement,(err,row,fields)=>{
     if(err){ 
         console.log(err);
         }
@@ -141,13 +149,54 @@ router.get("/vaccineClinics", function(req, res, next) {
                 allData[r]=row[r];
             }
 
-            res.send(JSON.stringify(allData));
-            console.log(row[0])
+            res.send(JSON.stringify(row));
         }
     });
     conn.end();
     
 });
+
+router.post("/appointmentTable", function(req, res, next) {
+
+    let appointmentInfo = req.body.AppointmentInfo;
+    hcId = appointmentInfo.healthCardId;
+    address = appointmentInfo.address;
+    date = appointmentInfo.date;
+    console.log(hcId);
+    console.log(address);
+    console.log(date);
+
+    
+
+    let conn=newConnection();
+    conn.connect();
+
+    conn.query(`INSERT INTO vaccineAppointment
+    VALUES ("${date}","${address}")`);
+
+    conn.query(`INSERT INTO registration
+    VALUES ("${hcId}","${date}")`);
+
+    let queryStatement = `SELECT p.personName, v.injectionDate, v.address From Person p, vaccineappointment v
+                WHERE p.healthCardId = "${hcId}" AND v.address = "${address}" AND v.injectionDate = "${date}"`;
+
+    conn.query(queryStatement,(err,row,fields)=>{
+    if(err){ 
+        }
+        else{
+            let allData={};
+
+            for(r in row){
+                allData[r]=row[r];
+            }
+
+            res.send(JSON.stringify(row));
+        }
+    });
+    conn.end();
+});
+
+
 
 
 
