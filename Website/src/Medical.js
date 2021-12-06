ba18fce3e825de7251fad2fbc665c818c0f0886f
nbroeder <nbroeder@uwo.ca>
2021-12-06 11:09:09 -0500

    Added a filter system for Medical page

--- Website/src/Medical.js
+++ Website/src/Medical.js
@@ -1,9 +1,9 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import HospitalTable from "./Components/Tables/HospitalTable.js"
 import HealthCareCentreTable from "./Components/Tables/HealthCareCentreTable"
 import VaccineClinicTable from "./Components/Tables/VaccineClinicTable"
 
-
+import Button from '@mui/material/Button';
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
@@ -13,91 +13,129 @@ import Divider from "@mui/material/Divider";
 import TextField from '@mui/material/TextField';
 function Tables(props) {
   const tableNum = props.tableState;
-  if (tableNum==0) {
+  if (tableNum == 0) {
     return <HealthCareCentreTable HCC={props.apiTable}></HealthCareCentreTable>;
-  }else if(tableNum==1){
+  } else if (tableNum == 1) {
     return <VaccineClinicTable clinicArray={props.apiTable}></VaccineClinicTable>
-  }else{
+  } else {
     return <HospitalTable hospitalArray={props.apiTable}></HospitalTable>;
   }
-  
+
 }
 
 function Medical() {
- 
-    const [apiResponse, setResponse] = useState([]);
-    const [tableState, setTableState] = useState(0);
 
-    useEffect(() => {
-        getDefaultTable();
-    }, []);
+  const [apiResponse, setResponse] = useState([]);
+  const [tableState, setTableState] = useState(0);
+  const [cityNameState, setCityState] = useState('');
 
-    function getDefaultTable() {
-        fetch("http://localhost:9000/testAPI/getHealthCentres")
-            .then(res => res.text())
-            .then(res => setResponse(JSON.parse(res)));
-          setTableState(0);
-           
-      }
-      function getClinics() {
-        fetch("http://localhost:9000/testAPI/getClinics")
-            .then(res => res.text())
-            .then(res => setResponse(JSON.parse(res)));
-            setTableState(1);
-      }
-      function getHospitals() {
-        fetch("http://localhost:9000/testAPI/getHospitals")
-            .then(res => res.text())
-            .then(res => setResponse(JSON.parse(res)));
-            setTableState(2);
-      }
-      return(
+  useEffect(() => {
+    getDefaultTable();
+  }, []);
 
-        
-        <Box >
-        <Grid container spacing={1}>
-          <Grid item xs={1} > 
-              <Box sx={{ bgcolor: '#ff9800', height: '100vh' }} />
-          </Grid>
+  function getDefaultTable() {
+    fetch("http://localhost:9000/testAPI/initializeTables" + cityNameState)
+        .then(res => res.text())
+        .then(res => setResponse(JSON.parse(res)));
+
+  }
+  function getHCC() {
+    setTableState(0);
+    fetch("http://localhost:9000/testAPI/getHealthCentres?city=" + cityNameState)
+        .then(res => res.text())
+        .then(res => setResponse(JSON.parse(res)));
+
+  }
+  function getClinics() {
+    setTableState(1);
+    fetch("http://localhost:9000/testAPI/getClinics?city=" + cityNameState)
+        .then(res => res.text())
+        .then(res => setResponse(JSON.parse(res)));
+
+  }
+  function getHospitals() {
+    setTableState(2);
+    fetch("http://localhost:9000/testAPI/getHospitals?city=" + cityNameState)
+        .then(res => res.text())
+        .then(res => setResponse(JSON.parse(res)));
+  }
   
-          <Grid item xs={10}>
-          <Container >
-          <List>
-  
-          <ListItem >
-           
 
-        <button onClick={getDefaultTable}>All</button>
-        <button onClick={getClinics}>Clinics</button>
-        <button onClick={getHospitals}>Hospitals</button>
+  function getHCCByCity() {
+    if (tableState == 0) {
+      fetch("http://localhost:9000/testAPI/getHealthCentres?city=" + cityNameState)
+        .then(res => res.text())
+        .then(res => setResponse(JSON.parse(res)));
 
-          
+    } else if (tableState == 1) {
+      fetch("http://localhost:9000/testAPI/getClinics?city=" + cityNameState)
+        .then(res => res.text())
+        .then(res => setResponse(JSON.parse(res)));
 
-        
-        </ListItem>
-        <Divider light />
-        
-        <ListItem >
-          Filter by City:
-        <TextField
-                label="City"
-            />
-        </ListItem>
-        <Divider light />
-        <ListItem>
-          <Tables tableState={tableState} apiTable={apiResponse}></Tables>
-        </ListItem>
-          </List>
-        </Container>
-          </Grid>
-          <Grid item xs ={1}>
+    } else {
+      fetch("http://localhost:9000/testAPI/getHospitals?city=" + cityNameState)
+        .then(res => res.text())
+        .then(res => setResponse(JSON.parse(res)));
+
+    }
+  }
+  return (
+
+
+    <Box >
+      <Grid container spacing={1}>
+        <Grid item xs={1} >
           <Box sx={{ bgcolor: '#ff9800', height: '100vh' }} />
-          </Grid>
         </Grid>
-      </Box>
-      )
 
-    
+        <Grid item xs={10}>
+          <Container >
+            <List>
+
+              <ListItem >
+
+
+                <Button onClick={getHCC}>All</Button>
+                <Button onClick={getClinics}>Clinics</Button>
+                <Button onClick={getHospitals}>Hospitals</Button>
+
+
+
+
+              </ListItem>
+              <Divider light />
+              <ListItem >
+                Add or remove healthcare centres:
+                <TextField
+                  label="healthcare centre"
+                />
+                <Button>Submit</Button>
+              </ListItem>
+              <Divider light />
+              <ListItem >
+                Filter by City:
+                <TextField
+                value={cityNameState}
+                  label="City"
+                  onChange={(e)=>{setCityState(e.target.value)}}
+                />
+                <Button onClick={getHCCByCity}>Submit</Button>
+              </ListItem>
+              <Divider light />
+              <ListItem>
+                <Tables tableState={tableState} apiTable={apiResponse}></Tables>
+              </ListItem>
+            </List>
+          </Container>
+        </Grid>
+        <Grid item xs={1}>
+          <Box sx={{ bgcolor: '#ff9800', height: '100vh' }} />
+        </Grid>
+      </Grid>
+    </Box>
+  )
+
+
 }
 
 
--- assignment4/api/routes/TestAPI.js
+++ assignment4/api/routes/TestAPI.js
@@ -31,93 +31,205 @@ router.get("/covidVarients", function(req, res, next) {
 });
 
 ///////////////////////get requests for the medical section\\\\\\\\\\\\\\\\\\\\\\\\\\
-router.get("/getHealthCentres", function(req, res, next) {
+router.get("/initializeTables", function(req, res, next) {
     let conn=newConnection();
     conn.connect();
-        
-    conn.query(`select hcc.address, c.cityName, v.spotName from healthcarecentre as hcc 
-    JOIN city as c ON hcc.UNLOCode=c.UNLOCode Left Join (select clinicName as spotName, address from vaccineclinic  
-    UNION select hospitalName, address from hospital) as v on v.address=hcc.address; `,(err,row,fields)=>{
+    conn.query(`
+    Drop View HCCInfo, hospitalInfo, clinicInfo; `,(err,row,fields)=>{
     if(err){
         console.log(err);
         }
-        else{
-            let allData={};
-            for(r in row){
-                allData[r]=row[r];
-            }
-            res.send(JSON.stringify(row));
-        }
+        
     });
+    conn.query(`Create view HCCInfo(address,cityName,spotName)
+    AS select hcc.address, c.cityName, v.spotName from healthcarecentre as hcc 
+        JOIN city as c ON hcc.UNLOCode=c.UNLOCode Left Join (select clinicName as spotName, address from vaccineclinic  
+        UNION select hospitalName, address from hospital) as v on v.address=hcc.address; `,(err,row,fields)=>{
+    if(err){
+        console.log(err);
+        }
+        
+    });
+    conn.query(`Create view clinicInfo(address,clinicName,cityName)
+    AS SELECT v.address, v.clinicName,c.cityName  FROM vaccineclinic AS v
+            JOIN healthcarecentre AS hcc
+            ON v.address=hcc.address
+            JOIN city AS c
+            ON hcc.UNLOCode=c.UNLOCode; `,(err,row,fields)=>{
+    if(err){
+        console.log(err);
+        }
+        
+    });
+    conn.query(`Create view hospitalInfo(address,hospitalName,bedsAvailable,numOfCovidCases,cityName)
+    AS SELECT h.address, h.hospitalName, h.bedsAvailable,h.numOfCovidCases,c.cityName  FROM hospital AS h
+        JOIN healthcarecentre AS hcc
+        ON h.address=hcc.address
+        JOIN city AS c
+        ON hcc.UNLOCode=c.UNLOCode;`,(err,row,fields)=>{
+    if(err){
+        console.log(err);
+        }
+        
+    });
+    conn.query(`SELECT * FROM HCCInfo`,(err,row,fields)=>{
+        if(err){
+            console.log(err);
+            }else{
+            
+                res.send(JSON.stringify(row));
+            }
+            
+        })
     conn.end();
     
 });
+
+router.get("/getHealthCentres", function(req, res, next) {
+    
+    if(req.query.city==""){
+       
+        let conn=newConnection();
+        conn.connect();
+            
+        conn.query(`Select * from HCCInfo;`,(err,row,fields)=>{
+        if(err){ 
+            console.log(err);
+            }
+            else{
+                let allData={};
+                for(r in row){
+                    allData[r]=row[r];
+                }
+                res.send(JSON.stringify(row));
+            }
+        });
+        conn.end();
+    }else{
+        let conn=newConnection();
+        conn.connect();
+            
+        conn.query(`Select * from HCCInfo
+        Where cityName LIKE "`+req.query.city+`%";`,(err,row,fields)=>{
+        if(err){ 
+            console.log(err);
+            }
+            else{
+                let allData={};
+                for(r in row){
+                    allData[r]=row[r];
+                }
+                res.send(JSON.stringify(row));
+            }
+        });
+        conn.end();
+    }
+        
+    });
+
 
 router.get("/getHospitals", function(req, res, next) {
-    let conn=newConnection();
-    conn.connect();
-        
-    conn.query(`SELECT h.address, h.hospitalName, h.numOfCovidCases,h.bedsAvailable,c.cityName  FROM hospital AS h
-    JOIN healthcarecentre AS hcc
-    ON h.address=hcc.address
-    JOIN city AS c
-    ON hcc.UNLOCode=c.UNLOCode;`,(err,row,fields)=>{
-    if(err){ 
-        console.log(err);
-        }
-        else{
-            let allData={};
-            for(r in row){
-                allData[r]=row[r];
-            }
-            res.send(JSON.stringify(row));
-        }
-    });
-    conn.end();
     
-});
+    if(req.query.city==""){
+       
+        let conn=newConnection();
+        conn.connect();
+            
+        conn.query(`Select * from hospitalInfo;`,(err,row,fields)=>{
+        if(err){ 
+            console.log(err);
+            }
+            else{
+                let allData={};
+                for(r in row){
+                    allData[r]=row[r];
+                }
+                res.send(JSON.stringify(row));
+            }
+        });
+        conn.end();
+    }else{
+        let conn=newConnection();
+        conn.connect();
+            
+        conn.query(`Select * from hospitalInfo
+        Where cityName LIKE "`+req.query.city+`%";`,(err,row,fields)=>{
+        if(err){ 
+            console.log(err);
+            }
+            else{
+                let allData={};
+                for(r in row){
+                    allData[r]=row[r];
+                }
+                res.send(JSON.stringify(row));
+            }
+        });
+        conn.end();
+    }
+        
+    });
 
 router.get("/getClinics", function(req, res, next) {
-    let conn=newConnection();
-    conn.connect();
-        
-    conn.query(`SELECT v.address, v.clinicName,c.cityName  FROM vaccineclinic AS v
-    JOIN healthcarecentre AS hcc
-    ON v.address=hcc.address
-    JOIN city AS c
-    ON hcc.UNLOCode=c.UNLOCode;`,(err,row,fields)=>{
-    if(err){ 
-        console.log(err);
-        }
-        else{
-            let allData={};
-            for(r in row){
-                allData[r]=row[r];
-            }
-            res.send(JSON.stringify(row));
-        }
-    });
-    conn.end();
     
-});
-
-router.get("/cities", function(req, res, next) {
-    let conn=newConnection();
-    conn.connect();
-        
-    conn.query("SELECT cityName FROM city",(err,row,fields)=>{
-    if(err){ 
-        console.log(err);
-        }
-        else{
-            let allData={};
-
-            for(r in row){
-                allData[r]=row[r];
+    if(req.query.city==""){
+       
+        let conn=newConnection();
+        conn.connect();
+            
+        conn.query(`Select * from clinicInfo;`,(err,row,fields)=>{
+        if(err){ 
+            console.log(err);
             }
+            else{
+                let allData={};
+                for(r in row){
+                    allData[r]=row[r];
+                }
+                res.send(JSON.stringify(row));
+            }
+        });
+        conn.end();
+    }else{
+        let conn=newConnection();
+        conn.connect();
+            
+        conn.query(`Select * from clinicInfo
+        Where cityName LIKE "`+req.query.city+`%";`,(err,row,fields)=>{
+        if(err){ 
+            console.log(err);
+            }
+            else{
+                let allData={};
+                for(r in row){
+                    allData[r]=row[r];
+                }
+                res.send(JSON.stringify(row));
+            }
+        });
+        conn.end();
+    }
+        
+    });
 
-            res.send(JSON.stringify(allData));
-            console.log(row[0])
+    //////////////MEDICAL END\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    router.get("/cities", function(req, res, next) {
+        let conn=newConnection();
+        conn.connect();
+            
+        conn.query("SELECT cityName FROM city",(err,row,fields)=>{
+        if(err){ 
+            console.log(err);
+            }
+            else{
+                let allData={};
+
+                for(r in row){
+                    allData[r]=row[r];
+                }
+
+                res.send(JSON.stringify(allData));
+                console.log(row[0]);
         }
     });
     conn.end();
