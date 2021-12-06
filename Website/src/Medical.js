baa7ad4c0f5530c3742df0dcb0609291e98cff35
nbroeder <nbroeder@uwo.ca>
2021-12-05 23:50:27 -0500

    Fixed the page layout and begain to add filtering functionality

--- Website/src/Medical.js
+++ Website/src/Medical.js
@@ -3,7 +3,15 @@ import HospitalTable from "./Components/Tables/HospitalTable.js"
 import HealthCareCentreTable from "./Components/Tables/HealthCareCentreTable"
 import VaccineClinicTable from "./Components/Tables/VaccineClinicTable"
 
-function Table(props) {
+
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import TextField from '@mui/material/TextField';
+function Tables(props) {
   const tableNum = props.tableState;
   if (tableNum==0) {
     return <HealthCareCentreTable HCC={props.apiTable}></HealthCareCentreTable>;
@@ -24,12 +32,7 @@ function Medical() {
         getDefaultTable();
     }, []);
 
-    
-    
-
-    
-
-      function getDefaultTable() {
+    function getDefaultTable() {
         fetch("http://localhost:9000/testAPI/getHealthCentres")
             .then(res => res.text())
             .then(res => setResponse(JSON.parse(res)));
@@ -50,15 +53,48 @@ function Medical() {
       }
       return(
 
-        <div id ="message container"> 
+        
+        <Box >
+        <Grid container spacing={1}>
+          <Grid item xs={1} > 
+              <Box sx={{ bgcolor: '#ff9800', height: '100vh' }} />
+          </Grid>
+  
+          <Grid item xs={10}>
+          <Container >
+          <List>
+  
+          <ListItem >
+           
 
         <button onClick={getDefaultTable}>All</button>
         <button onClick={getClinics}>Clinics</button>
         <button onClick={getHospitals}>Hospitals</button>
 
-          <Table tableState={tableState} apiTable={apiResponse}></Table>
+          
 
-        </div>
+        
+        </ListItem>
+        <Divider light />
+        
+        <ListItem >
+          Filter by City:
+        <TextField
+                label="City"
+            />
+        </ListItem>
+        <Divider light />
+        <ListItem>
+          <Tables tableState={tableState} apiTable={apiResponse}></Tables>
+        </ListItem>
+          </List>
+        </Container>
+          </Grid>
+          <Grid item xs ={1}>
+          <Box sx={{ bgcolor: '#ff9800', height: '100vh' }} />
+          </Grid>
+        </Grid>
+      </Box>
       )
 
     
