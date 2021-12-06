0f308486095b645c851a7317e1adf243c7e2e640
Robert <49911127+rfreeman203@users.noreply.github.com>
2021-12-06 10:09:14 -0500

    Update Table2.js

--- Website/src/Components/Tables/Table2.js
+++ Website/src/Components/Tables/Table2.js
@@ -1,5 +1,18 @@
+import React, { Component } from "react";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Output from '../../Output.json';
+
 import { render } from "@testing-library/react";
 import { Typography } from "@mui/material";
+
+
+import Table2 from '@mui/material/Table';
 
 //Displays Name, Vaccine, Clinic info
 
@@ -10,35 +23,51 @@ export default class BasicTable extends Component{
  {
  
     const length = Object.keys(Output).length
- 
+   
 
     return (
 
-        
-        <TableContainer component={Paper}>
-        <Table sx={{minWidth: 650}} aria-label="customized table">
-            <TableHead>
-                <TableRow>
-                    <TableCell align="right">Name</TableCell>
-                    <TableCell align="right">Date</TableCell>
-                    <TableCell align="right">Address</TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {apiAppointmentResponse.map((row, i) => (
-                    <TableRow
-                        key={i}
-                        sx={{'&:last-child td, &:last-child th': {border: 0}}}
-                    >
-                        <TableCell align="right">{apiAppointmentResponse[i].personName}</TableCell>
-                        <TableCell align="right">{apiAppointmentResponse[i].injectionDate}</TableCell>
-                        <TableCell align="right">{apiAppointmentResponse[i].address}</TableCell>
-                    </TableRow>
-                )
-                )}
-            </TableBody>
-        </Table>
-    </TableContainer>
+      <TableContainer style={{
+        alignItems: 'center',
+        justifyContent: 'center',
+        display:'flex'
+        }} 
+component={Paper}>
+<Table2  sx={{maxWidth: 1000}}aria-label="customized table">
+     <TableHead>
+       <TableRow>
+         
+         <TableCell>Name</TableCell>
+         <TableCell >City</TableCell>
+         <TableCell >Address</TableCell>
+         <TableCell >Beds Available</TableCell>
+         <TableCell >Active Cases</TableCell>
+         
+       </TableRow>
+     </TableHead>
+     <TableBody> 
+       {this.props.hospitalArray.map((row, i) => (
+         <TableRow
+           key={i}
+           sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+         >
+          <TableCell  align="left" component="th" scope="row">
+                 {this.props.hospitalArray[i].hospitalName}</TableCell>
+           <TableCell  align="left" component="th" scope="row">
+                 {this.props.hospitalArray[i].cityName}</TableCell>
+           <TableCell  align="left" component="th" scope="row">
+                 {this.props.hospitalArray[i].address}</TableCell>
+                 <TableCell  align="left" component="th" scope="row">
+                 {this.props.hospitalArray[i].bedsAvailable}</TableCell>
+           <TableCell  align="left" component="th" scope="row">
+                 {this.props.hospitalArray[i].numOfCovidCases}</TableCell>
+
+         </TableRow>
+       ))}
+     </TableBody>
+   </Table2>
+ </TableContainer>
+    
   );
 }
 }
